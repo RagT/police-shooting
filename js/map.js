12eb81230842a798f12d7e3d681752ec336a36b0
RagT <raghutir@gmail.com>
2015-07-05 23:37:40 -0700

    Addition of map

--- js/map.js
+++ js/map.js
@@ -1,32 +1,36 @@
-// Function to draw your map
-var drawMap() = function() {
 
-  // Create map and set viewd
- 
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+var drawMap = function() {
+  var map = L.map('container');
+  map.setView([47, -100], 3);
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+  layer.addTo(map);
+  getData(map);
 }
 
-// Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+var getData = function(map) {
+  $.ajax({url: "data/response.json", success: function(response){
+  		customBuild(response, map);
+  	},
+	error: function(){
+		$('#container').html("There has been an error getting your data.");
+	}});
 }
 
-// Do something creative with the data here!  
-var customBuild = function() {
-
-  
+var customBuild = function(response, map) {
+	var arr = $.parseJSON(response);
+	for(var i = 0; i < arr.length; i++){
+		var source = arr[i]['Source Link'];
+		var summary = arr[i]['Summary'];
+		var circColor = 'yellow';
+		if(arr[i]['Hit or Killed?'] == 'Killed'){
+			circColor = 'red';
+		}
+		var circle = L.circleMarker([arr[i].lat, arr[i].lng], {
+			color: circColor,
+			fillColor: circColor 
+		}).addTo(map);
+		circle.bindPopup(summary+ " <a target=\"blank\" href="+source+">Full Story</a>");
+	}
 }
 
 
